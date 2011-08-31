379283b27092f82d174e1b5f193be4a01d0a6540
FND <fnd@fnd-oda.(none)>
2011-08-31 18:40:04 +0200

    added demo page
    this is largely based on GitHub's original `preview.md`
    
    moved sample text from JavaScript into HTML
    
    in the process, changed whitespace in `preview.js` (tabs for
    indentation)
    
    no styling (yet?)

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,4 +1,4 @@
-$(function() {
+jQuery(function($) {
   var converter = new Showdown.converter();
   $("#user_input").keyup(function(){
     var txt = $("#user_input").val();
@@ -7,10 +7,7 @@ $(function() {
     $("#html_result").val(html.replace(/>/g, ">\n").replace(/</g, "\n<").replace(/\n{2,}/g, "\n\n"));
   });
 
-  var sample = "#### Underscores\nthis should have _emphasis_\nthis_should_not\n_nor_should_this\n\n\
-#### Autolinking\na non-markdown link: http://github.com/blog\nthis one is [a markdown link](http://github.com/blog)\nEmail test: support@github.com\n\n\
-#### Commit links\nc4149e7bac80fcd1295060125670e78d3f15bf2e\ntekkub@c4149e7bac80fcd1295060125670e78d3f15bf2e\nmojombo/god@c4149e7bac80fcd1295060125670e78d3f15bf2e\n\n\
-#### Issue links\nissue #1\ntekkub#1\nmojombo/god#1";
+  var sample = $("#sample").hide().text();
   $("#user_input").text(sample);
   var html = converter.makeHtml(sample);
   $("#result").html(html);
