b15f86ff2e342dfc6608c500f05c7b1c3e6abcd0
FND <fnd@fnd-oda.(none)>
2011-08-31 18:46:49 +0200

    fixed stylistic issues
    using tabs for indentation, adjusted whitespace and line lengths
    
    (only in `preview.js` as that's largely independent; for `showdown.js`
    itself, we'll stick with the original authors' style)

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,15 +1,17 @@
 jQuery(function($) {
-  var converter = new Showdown.converter();
-  $("#user_input").keyup(function(){
-    var txt = $("#user_input").val();
-    var html = converter.makeHtml(txt);
-    $("#result").html(html)
-    $("#html_result").val(html.replace(/>/g, ">\n").replace(/</g, "\n<").replace(/\n{2,}/g, "\n\n"));
-  });
+	var converter = new Showdown.converter();
+	$("#user_input").keyup(function() {
+		var txt = $("#user_input").val();
+		var html = converter.makeHtml(txt);
+		$("#result").html(html);
+		$("#html_result").val(html.replace(/>/g, ">\n").replace(/</g, "\n<").
+				replace(/\n{2,}/g, "\n\n"));
+	});
 
-  var sample = $("#sample").hide().text();
-  $("#user_input").text(sample);
-  var html = converter.makeHtml(sample);
-  $("#result").html(html);
-  $("#html_result").val(html.replace(/>/g, ">\n").replace(/</g, "\n<").replace(/\n{2,}/g, "\n\n"));
+	var sample = $("#sample").hide().text();
+	$("#user_input").text(sample);
+	var html = converter.makeHtml(sample);
+	$("#result").html(html);
+	$("#html_result").val(html.replace(/>/g, ">\n").replace(/</g, "\n<").
+			replace(/\n{2,}/g, "\n\n"));
 });
